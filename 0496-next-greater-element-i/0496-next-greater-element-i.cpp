bfd8b722155b15c78f1049de878fbe6ef3d6f3b9
harshitkumarsmps <harshitkumarsmps@gmail.com>
2026-07-24 18:53:26 +0530

    Time: 3 ms (40.74%), Space: 12.9 MB (39.88%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-   
-
-     int fn(vector<int>nums2, int i){
-         bool s=true; 
-         int ans=-1;
-        for(int a:nums2){
-            if(s==false&&a>i){
-                ans=a;
-                break;
-                
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        unordered_map<int,int>mp;
+        vector<int>vec;
+        int n = nums2.size();
+for (int i = n - 1; i >= 0; i--){
+            while(!st.empty()&& nums2[i]>=st.top()){
+                st.pop();
+        
             }
-            if(a==i)s=false;
+          
+            mp[nums2[i]]=st.empty()?-1:st.top();
+              st.push(nums2[i]);
             
         }
-        return ans;
-     }
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>vec;
+        for(int i:nums1)
+        vec.push_back(mp[i]);
 
 
-        for(int i:nums1){
-            vec.push_back(fn(nums2,i));
-
-        }
         return vec;
         
     }
