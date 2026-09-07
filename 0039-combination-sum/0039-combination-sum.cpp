a1a6654d7d40b08338469ca2a9eb66de595ed513
harshitkumarsmps <harshitkumarsmps@gmail.com>
2026-09-07 22:12:09 +0530

    Time: 52 ms (9.57%), Space: 45.2 MB (9.51%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
+    void fn(vector<vector<int>>&ans,vector<int>vec,int target,vector<int>&candidates,int ind){
+            if(ind==candidates.size()){
+                if(target==0){
+                    ans.push_back(vec);
+                  
+                }
+                return;
+            }
+            if(candidates[ind]<=target){
+                vec.push_back(candidates[ind]);
+                fn(ans,vec,target-candidates[ind],candidates,ind);
+                vec.pop_back();
 
-    void solve(int idx, vector<int>& candidates, int target,
-               vector<int>& ds, vector<vector<int>>& ans)
-    {
-        // Base case
-        if (idx == candidates.size())
-        {
-            if (target == 0)
-                ans.push_back(ds);
+                
 
-            return;
+            }
+                        fn(ans,vec,target,candidates,ind+1);
+
         }
-
-        // Take current element
-        if (candidates[idx] <= target)
-        {
-            ds.push_back(candidates[idx]);
-
-            // Stay at same index because same number can be reused
-            solve(idx, candidates, target - candidates[idx], ds, ans);
-
-            // Backtrack
-            ds.pop_back();
-        }
-
-        // Don't take current element
-        solve(idx + 1, candidates, target, ds, ans);
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
-    {
-        vector<vector<int>> ans;
-        vector<int> ds;
-
-        solve(0, candidates, target, ds, ans);
-
-        return ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        int ind=0;
+        vector<int>vec;
+         fn(ans,vec,target,candidates,ind);
+        return  ans;
+        
+    
     }
 };
