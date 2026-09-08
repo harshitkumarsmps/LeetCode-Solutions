4848a915afc5bb4b87c4908cf537610d411cd388
harshitkumarsmps <harshitkumarsmps@gmail.com>
2026-09-08 23:22:39 +0530

    Time: 0 ms (100%), Space: 117.9 MB (81.73%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,37 +11,35 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-          if (head == NULL || head->next == NULL)
-            return true;
 
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast!=NULL&& fast->next!=NULL){
-            slow=slow->next;
+         ListNode*slow=head;
+          ListNode*fast=head;
+          while(fast!=NULL&&fast->next!=NULL){
             fast=fast->next->next;
-        }
-        if(fast!=NULL){
             slow=slow->next;
-        }
-        ListNode*t1=slow;
-        ListNode*t2=NULL;
-        ListNode*t3=NULL;
-        while(t1!=NULL){
-            t2=t1->next;
-            t1->next=t3;
-            t3=t1;
-            t1=t2;
-        }
-        t1=head;
-        while(t3!=NULL){
-            if(t1->val!=t3->val)return false;
-            t3=t3->next;
-            t1=t1->next;
 
-        }
-        return true;
+          }
+          if(fast!=NULL){
+            slow=slow->next;
 
+          }
+          ListNode*t1=slow;
+           ListNode*t2=NULL;
+            ListNode*t3=NULL;
+            while(t1!=NULL){
+                t2=t1->next;
+                t1->next=t3;
+                t3=t1;
+                t1=t2;
 
+            }
+            while(t3!=NULL){
+                if(head->val!=t3->val)return false;
+                t3=t3->next;
+                head=head->next;
+
+            }
+    return true;
 
         
     }
